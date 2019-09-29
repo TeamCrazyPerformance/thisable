4610dcc38eda9e6899223f1de985e00084a43c8d
joi0104 <joi0104@naver.com>
2019-09-30 07:17:46 +0900

    add logout

--- app/src/main/java/com/tcp/thisable/MainActivity.java
+++ app/src/main/java/com/tcp/thisable/MainActivity.java
@@ -5,14 +5,19 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -130,10 +135,104 @@ public class MainActivity extends AppCompatActivity {
         });
 
         searchEdittext = findViewById(R.id.search_editText);
+        searchEdittext.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int io, KeyEvent keyEvent) {
+                if(io == EditorInfo.IME_ACTION_SEARCH){
+                    InputMethodManager mInputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                    mInputMethodManager.hideSoftInputFromWindow(searchEdittext.getWindowToken(), 0);
+
+                    double longitude = 126.97;
+                    double latitude = 37.56;
+                    if (mainMapFragment != null) {
+                        if (mainMapFragment.currentLocation != null) {
+                            longitude = mainMapFragment.currentLocation.longitude;
+                            latitude = mainMapFragment.currentLocation.latitude;
+                        }
+                    }
+
+                    int i = 1;
+                    for (ToggleButton toggle : advtoggle) {
+                        if (toggle.isChecked()) {
+                            list = dbHelper.getList(i);
+                        }
+                        i++;
+                    }
+
+                    String query = "";
+
+                    JSONObject jsonObject = new JSONObject();
+                    String[] keys = {"mainroad", "parking", "mainflat", "elevator", "toilet", "room", "seat", "ticket", "blind", "deaf", "guide", "wheelchair"};
+
+                    try {
+                        for (int j = 0; j < 12; j++)
+                        {
+                            if(list[j] == 1) {
+                                jsonObject.put(keys[j], true);
+                            }
+                        }
+
+                        Log.d("json", jsonObject.toString());
+                        query = jsonObject.toString();
+
+                    }
+
+                    catch (Exception e) {
+                        query = "{}";
+                    }
+
+
+                    Call<ArrayList<Data>> res = NetRetrofit.getInstance().getService().getSearchData("hotel", longitude, latitude, searchEdittext.getText().toString(), query);
+                    res.enqueue(new Callback<ArrayList<Data>>() {
+                        @Override
+                        public void onResponse(Call<ArrayList<Data>> call, Response<ArrayList<Data>> response) {
+                            Log.d("Retrofit", response.toString());
+                            if (response.body() != null) {
+                                listarray.clear();
+                                for (int i = 0; i < response.body().size(); i++) {
+                                    data = response.body().get(i);
+                                    listarray.add(data);
+                                }
+
+                                mainMapFragment = (MainMapFragment) getSupportFragmentManager().findFragmentByTag("MAP");
+                                mainListFragment = (MainListFragment) getSupportFragmentManager().findFragmentByTag("LIST");
+
+
+                                if(mainMapFragment != null) mainMapFragment.updateUi(listarray);
+
+                                if(mainListFragment != null) {
+                                    if(mainMapFragment != null) {
+                                        if(mainMapFragment.currentLocation != null) {
+                                            mainListFragment.updateUi(listarray, mainMapFragment.currentLocation);
+                                        }
+                                    }
+
+                                    else
+                                        mainListFragment.updateUi(listarray, null);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<ArrayList<Data>> call, Throwable t) {
+                            Log.d("Retrofit", t.toString());
+                        }
+                    });
+                    return true;
+                }
+                return false;
+            }
+        });
         searchButton = findViewById(R.id.search_button);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                InputMethodManager mInputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                mInputMethodManager.hideSoftInputFromWindow(searchEdittext.getWindowToken(), 0);
+
                 double longitude = 126.97;
                 double latitude = 37.56;
                 if (mainMapFragment != null) {
